1c9ebfab34ed9558c7c11b676677b30e042bee21
Marc Fournier <marc.fournier91@gmail.com>
2018-08-08 20:13:10 -0400

    Show data rendering in the dom

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -3,36 +3,23 @@ const {getConfig} = require('../../../backend/config')
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions/shared'
 class App extends React.Component {
-    state = {
-        message: null
-    }
-
-    api = getConfig("backendUri")+"api"
 
     componentDidMount() {
         console.log(this.props)
         const {dispatch} = this.props
 
         dispatch(handleInitialData())
-
-        // const urls = [`${this.api}/users`,`${this.api}/tweets`]
-        //
-        // var promises = urls.map(url => fetch(url).then(data => data.json()));
-        // Promise.all(promises).then(([users, tweets]) => {
-        //     // do something with results.
-        //     this.setState({
-        //         message: `${users[0].name}: ${tweets[0].text}`
-        //     })
-        // });
-
-
     }
 
     render() {
-        const {message} = this.state
+        const {tweets} = this.props
+        console.log("merp", Object.keys(tweets))
         return (
             <div>
-            {message || 'Loading'}
+            Your Timeline:
+            <ul>
+                {Object.keys(tweets).map((key) => <li key={key}>TWEET ID:{key}</li>)}
+            </ul>
     </div>
     )
     }
