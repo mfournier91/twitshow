fd37d68e42f9ac06b6136255bf7a1d474c5253b6
Marc Fournier <marc.fournier91@gmail.com>
2018-09-19 22:59:51 -0400

    Refactor tweet id list

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -1,32 +1,26 @@
 const React = require('react')
-const {getConfig} = require('../../../backend/config')
+
 import {connect} from 'react-redux'
 import {handleInitialData} from '../actions/shared'
+import Dashboard from './Dashboard'
+
 class App extends React.Component {
 
     componentDidMount() {
-        console.log(this.props)
         const {dispatch} = this.props
 
         dispatch(handleInitialData())
     }
-
+    
     render() {
-        const {tweets} = this.props
-        console.log("merp", Object.keys(tweets))
-        return (
-            <div>
-            Your Timeline:
-            <ul>
-                {Object.keys(tweets).map((key) => <li key={key}>TWEET ID:{key}</li>)}
-            </ul>
-    </div>
-    )
+      return (
+        <div>
+        {this.props.loading ? 'loading' : <Dashboard/>}
+        </div>
+      )
     }
 }
 
-export default connect((state) => ({
-    authedUser: state.authedUser,
-    users: state.users,
-    tweets: state.tweets
+export default connect(({authedUser}) => ({
+    loading: authedUser === null
 }))(App)
